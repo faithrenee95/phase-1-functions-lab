c24699e81ad759b11f8852917d9b1f5f2d2d159b
faith <faithreneemartin@gmail.com>
2022-06-07 13:32:14 -0400

    commit tree 14226744168e3fc91b1ca20f40aa9062bd263798

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = Math.abs(start - destination);
+    let feetTravelled = blocksTravelled * 264;
+    return feetTravelled;
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceFromHqInBlocks(blockNumber){
+    let hq = 42
+    return Math.abs(blockNumber - hq)
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = distanceTravelledInFeet(start, destination);
+    
+    if (feetTravelled <= 400) {
+        return 0;
+    }
+    else if (feetTravelled < 2000) {
+        return (feetTravelled - 400) * .02
+    }
+    else if (feetTravelled < 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far'
+    }
+    
+
+}
